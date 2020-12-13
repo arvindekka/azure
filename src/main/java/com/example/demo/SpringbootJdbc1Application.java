11f8d0b4f9f749a78ce9d8264337629b03b11fd3
arvind.ekka06@gmail.com <arvind.ekka06@gmail.com>
2020-12-13 22:07:55 +0530

    test

---
+++ src/main/java/com/example/demo/SpringbootJdbc1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbc1Application.class, args);
+	}
+
+}
